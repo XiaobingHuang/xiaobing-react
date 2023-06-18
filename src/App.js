eb1855fdffa1b1119432aada352fa01cdd68f852
xhuang35 <xhuang35@buffalo.edu>
2023-06-17 23:51:44 -0400

    side bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import IndexRouter from "./router/IndexRouter";
-
+import "./App.css";
 function App() {
   return (
     <div>
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+
+const { Sider} = Layout;
+
 
 function SideMenu() {
+    const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div>
-      SideMenu
-    </div>
+    <Sider trigger={null} collapsible >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
   )
+   
 }
 
 export default SideMenu
