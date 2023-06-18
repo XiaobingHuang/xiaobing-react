6ab95c97d0924b0c6ddce725bff243d5b8a3dedb
xhuang35 <xhuang35@buffalo.edu>
2023-06-18 16:19:48 -0400

    Layout

--- src/views/newsandbox/NewsSandBox.js
+++ src/views/newsandbox/NewsSandBox.js
@@ -7,23 +7,37 @@ import UserList from './user-manage/UserList'
 import RoleList from './right-manage/RoleList'
 import RightList from './right-manage/RightList'
 import NoPermission from './nopermisson/NoPermission'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
 
+const { Header, Sider, Content } = Layout;
 function NewsSandBox() {
   return (
     <div>
-       <SideMenu></SideMenu>
-       <TopHeader></TopHeader>
-       <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/user-manage/list" component={UserList} />
-            <Route path="/right-manage/role/list" component={RoleList} />
-            <Route path="/right-manage/right/list" component={RightList} />
-            <Redirect from="/" to="/home" exact />
-            <Route path="*" component={NoPermission}/>
-       </Switch>
-
+      <Layout>
+        <SideMenu></SideMenu>
+        <Layout>
+          <TopHeader></TopHeader>
+          <Content>
+            <Switch>
+              <Route path="/home" component={Home} />
+              <Route path="/user-manage/list" component={UserList} />
+              <Route path="/right-manage/role/list" component={RoleList} />
+              <Route path="/right-manage/right/list" component={RightList} />
+              <Redirect from="/" to="/home" exact />
+              <Route path="*" component={NoPermission} />
+            </Switch>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
-  )
+  );
 }
 
-export default NewsSandBox
+export default NewsSandBox;
