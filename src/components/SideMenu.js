d785fc629a82ca690d7f8980d58ec2f4533dcd97
xhuang35 <xhuang35@buffalo.edu>
2023-06-18 20:28:45 -0400

    side bar

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -3,8 +3,11 @@ import {
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
+  SettingOutlined
 } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
+import './index.css';
+
 
 const { Sider} = Layout;
 
@@ -13,32 +16,49 @@ function SideMenu() {
     const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Sider trigger={null} collapsible >
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-  )
+    <Sider trigger={null} collapsible collapsed={false}>
+      <div className="logo">
+        <strong>Global News Management System</strong>
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        items={[
+          {
+            key: "1",
+            icon: <UserOutlined />,
+            label: "nav 1",
+          },
+          {
+            key: "2",
+            icon: <VideoCameraOutlined />,
+            label: "nav 2",
+          },
+          {
+            key: "3",
+            icon: <UploadOutlined />,
+            label: "nav 3",
+          },
+          {
+            label: "User management",
+            key: "Sub4",
+            icon:<UploadOutlined />,
+            children: [
+              {
+                label: "Item 1",
+                key:"3-1"
+              },
+              {
+                label: "Item 2",
+                key:"3-2"
+              },
+            ],
+          },
+        ]}
+      />
+    </Sider>
+  );
    
 }
 
