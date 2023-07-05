437cdc08443ec6e8213306b528c93b2d78296ac3
xhuang35 <xhuang35@buffalo.edu>
2023-07-05 10:31:30 -0400

    map function

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -11,9 +11,62 @@ import './index.css';
 
 const { Sider} = Layout;
 
+const menu=[
+  {
+    key: "1",
+    icon: <UserOutlined />,
+    label: "nav 1",
+  },
+  {
+    key: "2",
+    icon: <VideoCameraOutlined />,
+    label: "nav 2",
+  },
+  {
+    key: "3",
+    icon: <UploadOutlined />,
+    label: "nav 3",
+  },
+  {
+    label: "User management",
+    key: "Sub4",
+    icon:<UploadOutlined />,
+    children: [
+      {
+        label: "Item 1",
+        key:"3-1"
+      },
+      {
+        label: "Item 2",
+        key:"3-2"
+      },
+    ],
+  },
+]
+
 
 function SideMenu() {
     const [collapsed, setCollapsed] = useState(false);
+    const menuList=(items)=>{ 
+      return items.map(item=>{
+        if(item.children){
+          return {
+            key:item.key,
+            icon: item.icon,
+            label:item.label,
+            children: menuList(item.children)
+          }
+        }
+        else{
+          return {
+            key:item.key,
+            icon: item.icon,
+            label:item.label
+          }
+        }
+      }
+      )
+    }
 
   return (
     <Sider trigger={null} collapsible collapsed={false}>
@@ -24,38 +77,7 @@ function SideMenu() {
         theme="dark"
         mode="inline"
         defaultSelectedKeys={["1"]}
-        items={[
-          {
-            key: "1",
-            icon: <UserOutlined />,
-            label: "nav 1",
-          },
-          {
-            key: "2",
-            icon: <VideoCameraOutlined />,
-            label: "nav 2",
-          },
-          {
-            key: "3",
-            icon: <UploadOutlined />,
-            label: "nav 3",
-          },
-          {
-            label: "User management",
-            key: "Sub4",
-            icon:<UploadOutlined />,
-            children: [
-              {
-                label: "Item 1",
-                key:"3-1"
-              },
-              {
-                label: "Item 2",
-                key:"3-2"
-              },
-            ],
-          },
-        ]}
+        items={menuList(menu)}
       />
     </Sider>
   );
